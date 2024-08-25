07e7084545422b0abfab7a954bf82e40a11939bd
Ali Deeb <ali1alideeb@gmail.com>
2024-08-25 16:02:44 +0300

    added splash.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,9 +43,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ae069ba2b839d5dafcacccf60f26dd3a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6c3be4d331a7b549314386834f1d6ccd",
-"/": "6c3be4d331a7b549314386834f1d6ccd",
-"main.dart.js": "0dc6082877ae911b016c0351367e9fc6",
+"index.html": "38223233b6a13b1dc50225fc12a606e3",
+"/": "38223233b6a13b1dc50225fc12a606e3",
+"main.dart.js": "940191c19cf73bd18c57cbcf8f4f73cb",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"};
 // The application shell files that are downloaded before a service worker can
